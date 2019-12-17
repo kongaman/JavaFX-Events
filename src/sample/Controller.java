792c6cfe246b1fd34b7ec6d71ee3fcc499254106
Chris <christian.kranz@gmail.com>
2019-12-17 11:32:04 +0100

    Events continued
    added ids to buttons
    added actionevent to buttonEventhandler
    added eventhandler for KeyReleased
    added initialize-method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,41 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField nameField;
+    @FXML
+    private Button btnHello;
+    @FXML
+    private Button btnBye;
 
     @FXML
-    public void onButtonClicked() {
-        System.out.println("Hello, " + nameField.getText());
+    public void initialize() { //is called automatically when application is being initialized, cannot have parameters
+        btnHello.setDisable(true);
+        btnBye.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if (e.getSource().equals(btnHello)) {
+            System.out.println("Hello, " + nameField.getText());
+            System.out.println("The following button wa pressed: " + e.getSource());
+        } else if (e.getSource().equals(btnBye)) {
+            System.out.println("Bye, " + nameField.getText());
+            System.out.println("The following button wa pressed: " + e.getSource());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButton = text.isEmpty() || text.trim().isEmpty();
+        btnHello.setDisable(disableButton);
+        btnBye.setDisable(disableButton);
+
     }
 }

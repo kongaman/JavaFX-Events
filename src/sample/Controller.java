6a87ab5c6e4847c020e9d0eff309bce4106a6303
Chris <christian.kranz@gmail.com>
2019-12-18 11:06:58 +0100

    implemented ui update on 2nd thread

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -15,6 +17,8 @@ public class Controller {
     private Button btnBye;
     @FXML
     private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
 
     @FXML
     public void initialize() { //is called automatically when application is being initialized, cannot have parameters
@@ -31,6 +35,32 @@ public class Controller {
             System.out.println("Bye, " + nameField.getText());
             System.out.println("The following button wa pressed: " + e.getSource());
         }
+
+        // Way to update ui elements that take a while to process (db for example) without locking/freezing the ui
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String s = (Platform.isFxApplicationThread()) ? "UI Thread" : "Background Thread";
+                    System.out.println("I'm going to sleep on the: " + s);
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() { // has to be done this way to run on the ui thread
+                        @Override
+                        public void run() {
+                            String s = (Platform.isFxApplicationThread()) ? "UI Thread" : "Background Thread";
+                            System.out.println("I'm updating the label on the: " + s);
+                            ourLabel.setText("We did something");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    // we do not care about this
+                }
+            }
+        };
+
+        new Thread(task).start();
+
+        // End ui-thread part
 
         if(ourCheckBox.isSelected()) {
             nameField.clear();
